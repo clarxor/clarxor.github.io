735c867b4ecf1e09d232d976e335e6313b1b3212
clarxor <clarkcwu@gmail.com>
2017-01-03 02:39:12 -0800

    Working

---
+++ slide.js
@@ -0,0 +1,30 @@
+
+var slideIndex = 1;
+//initial showing
+showDivs(slideIndex, "mySlides");
+showDivs(slideIndex, "mySlides2");
+
+function plusDivs(n, name) {
+  showDivs(slideIndex += n, name);
+}
+
+function currentDiv(n, name) {
+  showDivs(slideIndex = n, name);
+}
+
+function showDivs(n, name) {
+  var i;
+  //var x = document.getElementsByClassName("mySlides");
+  var x = document.getElementsByClassName(name);
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
+}
